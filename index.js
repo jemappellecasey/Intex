e3bca1565001d81278824501aa31514a9020a4aa
= <casey.lee.black@gmail.com>
2025-12-01 13:10:17 -0700

    Added in suggested page structure and function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // npm install express ejs knex pg express-session multer bcrypt helmet '@dr.pogodin/csurf' connect-flash nodemailer
+/*
+Participants
+Events
+Surveys (post)
+Milestones (you will need to support the milestones setup which can then be assigned to participants in a 1 to many relationship)
+Donations
+
+
+
+*/
